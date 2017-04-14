3cdb8e5764e6551d2ee9f43e201e4fb6b34a17c0
Abrahán Fernández Nieto <afernandez.nieto@atsistemas.com>
2017-04-14 12:45:22 +0200

    Initial commit

---
+++ GeniusFeedTests/GeniusFeedTests.swift
@@ -0,0 +1,36 @@
+//
+//  GeniusFeedTests.swift
+//  GeniusFeedTests
+//
+//  Created by Abrahán Fernández Nieto on 14/4/17.
+//  Copyright © 2017 Abrahanfer. All rights reserved.
+//
+
+import XCTest
+@testable import GeniusFeed
+
+class GeniusFeedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
